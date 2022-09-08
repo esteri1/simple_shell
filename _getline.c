5b30ab040ede40df455a6a90a54782becb7d832f
HYM <104323440+Haryohmi@users.noreply.github.com>
2022-09-08 10:00:15 +1000

    Update _getline.c

--- _getline.c
+++ _getline.c
@@ -4,7 +4,7 @@
 * _getline - read one line from the prompt.
 * @data: struct for the program's data
 *
-* Return: reading counting bytes.
+* Return: reading counting bytes..
 */
 int _getline(data_of_program *data)
 {
